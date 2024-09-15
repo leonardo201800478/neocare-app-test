f3092f3bbab7ad95c9d948d6389e3e4ec7c134e8
strife <201800478@unifoa.edu.br>
2024-09-15 09:08:11 -0300

    Update de código depreciado

--- powersync/AppSchema.ts
+++ powersync/AppSchema.ts
@@ -1,16 +1,23 @@
-import { column, Schema, TableV2 } from '@powersync/react-native';
+// powersync/AppSchema.ts
 
+import { column, Schema, Table } from '@powersync/react-native';
+
+// Nome da tabela
 export const TODOS_TABLE = 'todos';
 
-const todos = new TableV2({
+// Definindo a tabela com a nova classe Table
+const todos = new Table({
   task: column.text,
   user_id: column.text,
   is_complete: column.integer,
+  id: column.text, // Adicionando id se for necessário
 });
 
+// Criando o esquema com a tabela definida
 export const AppSchema = new Schema({
   todos,
 });
 
+// Definindo o tipo Database com base no esquema
 export type Database = (typeof AppSchema)['types'];
 export type Todo = Database['todos'];
--- powersync/PowerSync.tsx
+++ powersync/PowerSync.tsx
@@ -1,11 +1,9 @@
 import '@azure/core-asynciterator-polyfill';
+
 import 'react-native-polyfill-globals/auto';
 import { Kysely, wrapPowerSyncWithKysely } from '@powersync/kysely-driver';
-import {
-  AbstractPowerSyncDatabase,
-  RNQSPowerSyncDatabaseOpenFactory,
-} from '@powersync/react-native';
-import { createContext, useContext } from 'react';
+import { PowerSyncDatabase } from '@powersync/react-native';
+import React from 'react';
 
 import { AppSchema, Database } from './AppSchema';
 
@@ -13,17 +11,17 @@ import { SupabaseConnector } from '~/powersync/SupabaseConnector';
 
 export class System {
   supabaseConnector: SupabaseConnector;
-  powersync: AbstractPowerSyncDatabase;
+  powersync: PowerSyncDatabase;
   db: Kysely<Database>;
 
   constructor() {
-    const factory = new RNQSPowerSyncDatabaseOpenFactory({
-      schema: AppSchema,
-      dbFilename: 'app.sqlite',
-    });
-
     this.supabaseConnector = new SupabaseConnector();
-    this.powersync = factory.getInstance();
+    this.powersync = new PowerSyncDatabase({
+      schema: AppSchema,
+      database: {
+        dbFilename: 'app.sqlite',
+      },
+    });
     this.db = wrapPowerSyncWithKysely(this.powersync);
   }
 
@@ -35,5 +33,6 @@ export class System {
 }
 
 export const system = new System();
-export const SystemContext = createContext(system);
-export const useSystem = () => useContext(SystemContext);
+
+export const SystemContext = React.createContext(system);
+export const useSystem = () => React.useContext(SystemContext);
